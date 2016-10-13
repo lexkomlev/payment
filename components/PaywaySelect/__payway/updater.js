a2e2a06b472f8120152fdd212ac9c30f261fc347
shuranov <shuranov@tutu.ru>
2016-10-13 12:00:31 +0300

    action fix, initialState amended

--- components/PaywaySelect/__payway/updater.js
+++ components/PaywaySelect/__payway/updater.js
@@ -6,5 +6,5 @@ import { CLICK } from './actions';
 export const init = (state = 'card') => (state);
 
 export default new Updater(init())
-	.case(CLICK, (model, { payload }) => payload)
+	.case(CLICK, (model, { payload }) => payload )
 	.toReducer();
