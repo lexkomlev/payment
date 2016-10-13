caafa658800b7209179c8a452b655b83e6044373
shuranov <shuranov@tutu.ru>
2016-10-13 14:46:28 +0300

    added basic wrapper styles

--- components/Payment/view.jsx
+++ components/Payment/view.jsx
@@ -1,3 +1,5 @@
+import styles from './styles';
+
 import React from 'react';
 import { view, forwardTo } from 'redux-elm';
 
@@ -6,8 +8,9 @@ import Contacts from 'components/Contacts/view';
 import PaywaySelect from 'components/PaywaySelect/view';
 
 
+
 export default view(({ model, dispatch }) => (
-    <div>
+    <div className={styles.wrapper}>
         <TripInfo model={model.tripInfo} dispatch={forwardTo(dispatch, 'TripInfo')} />
         <br/>
         <Contacts model={model.contacts} dispatch={forwardTo(dispatch, 'Contacts')} />
